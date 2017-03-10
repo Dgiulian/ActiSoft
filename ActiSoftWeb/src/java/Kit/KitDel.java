a0bd2272eb3f9e272f9776426b3557de73dc6706
Diego Giuliani <giuliani.diego@gmail.com>
2017-03-10 13:43:03 -0300

    Ajustes en KitDel

--- ActiSoftWeb/src/java/Kit/KitDel.java
+++ ActiSoftWeb/src/java/Kit/KitDel.java
@@ -7,6 +7,7 @@ package Kit;
 import bd.Activo;
 import bd.Kit;
 import bd.Kit_detalle;
+import bd.Kit_historia;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,10 +21,12 @@ import transaccion.TActivo;
 import transaccion.TAuditoria;
 import transaccion.TKit;
 import transaccion.TKit_detalle;
+import transaccion.TKit_historia;
 import utils.BaseException;
 import utils.JsonRespuesta;
 import utils.OptionsCfg;
 import utils.Parser;
+import utils.TFecha;
 
 /**
  *
@@ -82,7 +85,7 @@ public class KitDel extends HttpServlet {
            Kit kit = new TKit().getById(id);            
            if (kit==null) throw new BaseException("ERROR","No existe el registro");
            
-           if(kit.getId_estado()==OptionsCfg.KIT_ESTADO_ALQUILADO){
+           if(OptionsCfg.KIT_ESTADO_ALQUILADO.equals(kit.getId_estado())){
                throw new BaseException("ERROR","El kit se encuentra alquilado y no puede ser eliminado");
            }
            kit.setActivo(0);
@@ -91,6 +94,7 @@ public class KitDel extends HttpServlet {
                TKit_detalle tkd =  new TKit_detalle();
                List<Kit_detalle> lstDetalle =tkd.getByKitId(kit.getId());
                TActivo ta = new TActivo();
+               TKit_historia th = new TKit_historia();
                for(Kit_detalle d:lstDetalle){
                     // Cuando se elimina un kit, hay que devolver el stock del activo.
                    Activo activo = ta.getById(d.getId_activo());
@@ -98,6 +102,12 @@ public class KitDel extends HttpServlet {
                     activo.setStock(activo.getStock() + 1);
                     activo.setId_estado(OptionsCfg.ACTIVO_ESTADO_DISPONIBLE);
                     ta.actualizar(activo);
+                    
+                    Kit_historia historia = new Kit_historia();
+                    historia.setId_activo(d.getId_activo());
+                    historia.setId_accion(OptionsCfg.ACCION_BAJA);
+                    historia.setFecha(TFecha.ahora(TFecha.formatoBD + " " + TFecha.formatoHora));
+                    th.alta(historia);
                    }
 //                   tkd.baja(d);
                }
